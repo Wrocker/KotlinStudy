4d4d3fdae81683837d65bad7467f68f6cce70ac3
rocker <rocker12580@hotmail.com>
2020-07-07 23:50:45 +0800

    泛型  函数  星投影

--- app/src/main/java/com/rocker/kotlinstudy/ui/activity/objects/GenericsActivity.kt
+++ app/src/main/java/com/rocker/kotlinstudy/ui/activity/objects/GenericsActivity.kt
@@ -1,5 +1,8 @@
 package com.rocker.kotlinstudy.ui.activity.objects
 
+import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
 import com.rocker.kotlinstudy.R
 import com.rocker.kotlinstudy.ui.activity.BaseLoadListActivity
 import com.rocker.kotlinstudy.ui.adapter.ContentLayoutAdapter
@@ -27,12 +30,27 @@ class GenericsActivity : BaseLoadListActivity() {
         data.add(ContentLayoutAdapter.LayType("     in 只可以被消费而不可以被生产。"))
         data.add(ContentLayoutAdapter.LayType("     只可以被消费而不可以被生产。"))
 
-        data.add(ContentLayoutAdapter.LayType("♦️  类型投影（type projections）"))
+        data.add(ContentLayoutAdapter.LayType("♦️  使用处型变：类型投影（type projections）"))
         data.add(ContentLayoutAdapter.LayType("     有些类既需要被生存，又需要被消费的情况//例如Array"))
+        data.add(ContentLayoutAdapter.LayType("     即out和in 应用于同一个类的方法中"))
+
+        data.add(ContentLayoutAdapter.LayType("♦️  星投影"))
+        data.add(ContentLayoutAdapter.LayType("     - 对于 Foo <out T : TUpper>，其中 T 是一个具有上界 TUpper 的协变类型参数，Foo <*> 等价于 Foo <out TUpper>。"))
+        data.add(ContentLayoutAdapter.LayType("       这意味着当 T 未知时，你可以安全地从 Foo <*> 读取 TUpper 的值。"))
+        data.add(ContentLayoutAdapter.LayType("     - 对于 Foo <in T>，其中 T 是一个逆变类型参数，Foo <*> 等价于 Foo <in Nothing>。"))
+        data.add(ContentLayoutAdapter.LayType("       这意味着当 T 未知时，没有什么可以以安全的方式写入 Foo <*>。"))
+        data.add(ContentLayoutAdapter.LayType("     - 对于 Foo <T : TUpper>，其中 T 是一个具有上界 TUpper 的不型变类型参数，Foo<*> 对于读取值时等价于 Foo<out TUpper> 而对于写值时等价于 Foo<in Nothing>。"))
+        data.add(ContentLayoutAdapter.LayType("      注意：星投影非常像 Java 的原始类型，但是安全。"))
 
         data.add(ContentLayoutAdapter.LayType("♦️  泛型函数"))
+        data.add(ContentLayoutAdapter.LayType("     要调用泛型函数，在调用处函数名之后指定类型参数即可："))
+        val l1 = singletonList<Int>(1)
+        data.add(ContentLayoutAdapter.LayType("     可以省略能够从上下文中推断出来的类型参数，所以以下示例同样适用："))
+        val l2 = singletonList(1)
 
         data.add(ContentLayoutAdapter.LayType("♦️  泛型约束"))
+        data.add(ContentLayoutAdapter.LayType("♦️  上界"))
+        data.add(ContentLayoutAdapter.LayType("♦️  多个上界，where语句"))
 
         data.add(ContentLayoutAdapter.LayType("♦️  类型擦除"))
 
@@ -45,6 +63,7 @@ class GenericsActivity : BaseLoadListActivity() {
         //如果类型参数可以推断出来，例如从构造函数的参数或者从其他途径，允许省略类型参数
         val box1 = Box(2)
 
+        //使用处型变
         val ints: Array<Int> = arrayOf(1, 2, 3)
         val anys = Array<Any>(3) { 3; 4 }
 //        copy(ints, any)//报错，copy时，Any和Int是不型变的
@@ -52,6 +71,17 @@ class GenericsActivity : BaseLoadListActivity() {
         val chars = Array<Char>(3){'1'}
         fill(anys, "haha")
 //        fill(chars, "haha")
+
+        //星投影
+        val list1 : Array<TextView> = arrayOf(rootBinding.tvTitle);
+        val list2 : Array<*> = list1
+        //这里Array<*>等价于下面的
+        val list3 : Array<out View> = list1
+        val list4 : Array<in CheckBox> = list1
+
+        //泛型约束
+//        sort(listOf(1, 2, 3)) // OK。Int 是 Comparable<Int> 的子类型
+//        sort(listOf(HashMap<Int, String>())) // 错误：HashMap<Int, String> 不是 Comparable<HashMap<Int, String>> 的子类型
     }
 
     class Box<T>(var t: T)
@@ -113,4 +143,24 @@ class GenericsActivity : BaseLoadListActivity() {
         dest.forEachIndexed { index, any -> if(any == null) dest[index] = value }
     }
 
+    //泛型函数
+    fun <T> singletonList(item: T): List<T> {
+        return arrayListOf(item)
+    }
+
+    fun <T> T.basicToString(): String {  // 扩展函数
+        return "hash:${hashCode()}"
+    }
+
+    val <T> List<T>.lastIndex: Int
+        get() = size - 1
+
+    //泛型约束 上界 //todo 同一个类中出现同名类
+    fun <T : Comparable<T>> sort(list: List<T>) {  } //只有 Comparable<T> 的子类型可以替代 T
+    //多个上界的情况
+    fun <T> copyWhenGreater(list: List<T>, threshold: T): List<String>
+            where T : CharSequence,
+                  T : Comparable<T> {
+        return list.filter { it > threshold }.map { it.toString() }
+    }
 }
