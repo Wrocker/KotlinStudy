b880d4f93b52fed373ceb90489734a7dff4dc1cc
rocker <rocker12580@hotmail.com>
2020-07-26 22:25:58 +0800

    Lambda表达式部分

--- app/src/main/java/com/rocker/kotlinstudy/ui/activity/function/LambdasActivity.kt
+++ app/src/main/java/com/rocker/kotlinstudy/ui/activity/function/LambdasActivity.kt
@@ -40,13 +40,61 @@ class LambdasActivity : BaseLoadListActivity() {
         data.add(ContentLayoutAdapter.LayType("         _构造函数：::Regex"))
         data.add(ContentLayoutAdapter.LayType("         这包括指向特定实例成员的绑定的可调用引用：foo::toString"))
         data.add(ContentLayoutAdapter.LayType("     - 使用实现函数类型接口的自定义类的实例"))
+        data.add(ContentLayoutAdapter.LayType("     有足够信息，编译器可以推断变量的函数类型"))
+        data.add(ContentLayoutAdapter.LayType("     带与不带接收者的函数类型非字面值可以互换，其中接收者可以替代第一个参数，反之亦然。"))
+        data.add(ContentLayoutAdapter.LayType("♦️  函数类型实例调用"))
+        data.add(ContentLayoutAdapter.LayType("     函数类型的值可以通过其 invoke(……) 操作符调用：f.invoke(x) 或者直接 f(x)。"))
+        data.add(ContentLayoutAdapter.LayType("     如果该值具有接收者类型，那么应该将接收者对象作为第一个参数传递"))
+        data.add(ContentLayoutAdapter.LayType("     调用带有接收者的函数类型值的另一个方式是在其前面加上接收者对象， 就好比该值是一个扩展函数：1.foo(2)"))
+        data.add(ContentLayoutAdapter.LayType("♦️  内联函数"))
 
-        data.add(ContentLayoutAdapter.LayType("♦️  解决覆盖冲突"))
+        data.add(ContentLayoutAdapter.LayType("♦️  Lambda 表达式与匿名函数"))
+        data.add(ContentLayoutAdapter.LayType("     lambda 表达式与匿名函数是“函数字面值”，即未声明的函数， 但立即做为表达式传递"))
+        data.add(ContentLayoutAdapter.LayType("♦️  Lambda 表达式语法"))
+        data.add(ContentLayoutAdapter.LayType("     Lambda 表达式的完整语法形式如下"))
+        data.add(ContentLayoutAdapter.LayType("     val sum: (Int, Int) -> Int = { x: Int, y: Int -> x + y }"))
+        data.add(ContentLayoutAdapter.LayType("     lambda 表达式总是括在花括号中， 完整语法形式的参数声明放在花括号内，并有可选的类型标注， 函数体跟在一个 -> 符号之后。"))
+        data.add(ContentLayoutAdapter.LayType("     如果推断出的该 lambda 的返回类型不是 Unit，那么该 lambda 主体中的最后一个（或可能是单个） 表达式会视为返回值。"))
+        data.add(ContentLayoutAdapter.LayType("     如果我们把所有可选标注都留下，看起来如下"))
+        data.add(ContentLayoutAdapter.LayType("     val sum = { x: Int, y: Int -> x + y }"))
+        data.add(ContentLayoutAdapter.LayType("♦️  传递末尾的 lambda 表达式"))
+        data.add(ContentLayoutAdapter.LayType("     拖尾 lambda 表达式：如果函数的最后一个参数是函数，那么作为相应参数传入的 lambda 表达式可以放在圆括号之外"))
+        data.add(ContentLayoutAdapter.LayType("     如果该 lambda 表达式是调用时唯一的参数，那么圆括号可以完全省略"))
+        data.add(ContentLayoutAdapter.LayType("♦️  it：单个参数的隐式名称"))
+        data.add(ContentLayoutAdapter.LayType("     如果编译器自己可以识别出签名，也可以不用声明唯一的参数并忽略 ->。 该参数会隐式声明为 it"))
+        data.add(ContentLayoutAdapter.LayType("♦️  从 lambda 表达式中返回一个值"))
+        data.add(ContentLayoutAdapter.LayType("     我们可以使用限定的返回语法从 lambda 显式返回一个值。 否则，将隐式返回最后一个表达式的值。"))
+        data.add(ContentLayoutAdapter.LayType("♦️  下划线用于未使用的变量（自 1.1 起）"))
+        //map.forEach { _, value -> println("$value!") }
 
 
+//
         adapter.data = data
         adapter.notifyDataSetChanged()
-        
+
+        //拖尾 lambda 表达式
+        doSth("haha"){
+                string ->
+            println("打印$string")
+            "print $string"
+        }
+
+        //max(strings, { a, b -> a.length < b.length })
+        //函数 max 是一个高阶函数，它接受一个函数作为第二个参数。 其第二个参数是一个表达式，它本身是一个函数，即函数字面值，它等价于以下具名函数：
+        //fun compare(a: String, b: String): Boolean = a.length < b.length
+
+        //实例的调用
+        //stringPlus是该Lambda表达式的名称
+        val stringPlus: (String, String) -> String = String::plus
+
+        println(stringPlus.invoke("<-", "->"))//f.invoke(x)
+        println(stringPlus("Hello, ", "world!"))//f(x)
+        //如果该值具有接收者类型，那么应该将接收者对象作为第一个参数传递。 调用带有接收者的函数类型值的另一个方式是在其前面加上接收者对象， 就好比该值是一个扩展函数：1.foo(2)
+        val intPlus: Int.(Int) -> Int = Int::plus
+        println(intPlus.invoke(1, 1))
+        println(intPlus(1, 2))
+        println(2.intPlus(3)) // 类扩展调用
+
         //右结合法 todo 例子
 //        doSth("右结合", {
 //            string
@@ -78,6 +126,43 @@ class LambdasActivity : BaseLoadListActivity() {
         val joinedToString = items.fold("Elements:", { acc, i -> "$acc $i" })
         // 函数引用也可以用于高阶函数调用
         val product = items.fold(1, Int::times)//Int::times的意思是 3.times(2)表示3的几倍
+
+        //Lambda表达式
+        //it：单个参数的隐式名称
+        items.filter { it > 0 } // 这个字面值是“(it: Int) -> Boolean”类型的
+        //以使用限定的返回语法从 lambda 显式返回一个值。 否则，将隐式返回最后一个表达式的值。
+        items.filter {
+            val shouldFilter = it > 0
+            shouldFilter
+        }
+        items.filter {
+            val shouldFilter = it > 0
+            return@filter shouldFilter
+        }
+        //可以使用限定的返回语法从 lambda 显式返回一个值。这一约定连同在圆括号外传递 lambda 表达式一起支持 LINQ-风格 的代码
+        items.filter { it > 0 }.sortedBy { it }.map { it.plus(2) }
+    }
+
+    //带与不带接收者的函数类型非字面值可以互换，其中接收者可以替代第一个参数，反之亦然。todo 没懂
+    //例如，(A, B) -> C 类型的值可以传给或赋值给期待 A.(B) -> C 的地方，反之亦然
+    //默认情况下推断出的是没有接收者的函数类型，即使变量是通过扩展函数引用来初始化的。 如需改变这点，请显式指定变量类型。
+    val repeatFun: String.(Int) -> String = { times -> this.repeat(times) }
+    val twoParameters: (String, Int) -> String = repeatFun // OK
+
+    fun runTransformation(f: (String, Int) -> String): String {
+        return f("hello", 3)
+    }
+
+    val result = runTransformation(repeatFun) // OK
+
+    //编译器可以推断变量的函数类型
+    val simpleA = {i: Int -> i + 5}
+
+    //使用实现函数类型接口的自定义类的实例
+    val intFunction: (Int) -> Int = IntTransformer()
+
+    class IntTransformer: (Int) -> Int {
+        override operator fun invoke(x: Int): Int = x + 1000
     }
 
     //高阶函数
